a92e2952499d8a551749fec862ba105320c83e41
karlheinz.niebuhr <karlheinz.niebuhr@gmx.com>
2015-06-03 15:09:40 -0400

    error handling

--- pyrawebs.py
+++ pyrawebs.py
@@ -6,11 +6,11 @@ import random
 from random import randint
 from time import sleep
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
+CONSUMER_KEY = 'vlXuFjArLxVUk2NnsMWMpemVQ'
+CONSUMER_SECRET = 'L1IKV1DUKKWNGzoBW7gMYx2OUHt6Bby6uJLyhtgX7OUrNigwwb'
 
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+ACCESS_TOKEN = '3234602466-e74DNGCXpMr3XYbuIwUd1xNOPDSeOxPCCCSwIzZ'
+ACCESS_TOKEN_SECRET = '8d8O2ZjuoTbHCoJqpYxd13IUGAPwOc2DOkuXjKtqxYhrX'
 
 def oauth_login(consumer_key, consumer_secret,access_key,access_secret):
   """Authenticate with twitter using OAuth"""
@@ -27,8 +27,6 @@ if __name__ == "__main__":
 
   arg_tweets  = str(sys.argv[1])
   arg_target  = str(sys.argv[2])
-  print arg_tweets
-  print arg_target
 
   f_tweets     = open(arg_tweets,'r')
   f_targets  = open(arg_target,'r')
@@ -36,7 +34,6 @@ if __name__ == "__main__":
   s_tweets     = f_tweets.readlines()
   s_targets  = f_targets.readlines()
   
-
   # crear lista de targets
   for line in s_targets:
     l_targets.append(line.strip().decode('utf-8'))
@@ -49,7 +46,7 @@ if __name__ == "__main__":
   # crear y enviar tweets
   for tweet in l_tweets:
     actual_target = '@'
-    for target in l_targets:
+    for target in l_targets[18:]:
       tweet   = tweet.replace(actual_target, target)
       if target != '':
         actual_target = target
@@ -60,6 +57,7 @@ if __name__ == "__main__":
         espera = randint(4,20)
         time.sleep(espera)
         print 'espera: '+str(espera)+' segundos'
-      except:
+      except Exception as exception:
+        print exception
         print "Failed to tweet:", tweet
       
