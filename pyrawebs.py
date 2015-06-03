856d884bf705240d563904493daf1c571d4b1156
karlheinz.niebuhr <karlheinz.niebuhr@gmx.com>
2015-06-03 14:44:03 -0400

    version mejorada

--- pyrawebs.py
+++ pyrawebs.py
@@ -6,11 +6,11 @@ import random
 from random import randint
 from time import sleep
 
-CONSUMER_KEY = 'vlXuFjArLxVUk2NnsMWMpemVQ'
-CONSUMER_SECRET = 'L1IKV1DUKKWNGzoBW7gMYx2OUHt6Bby6uJLyhtgX7OUrNigwwb'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
-ACCESS_TOKEN = '3234602466-e74DNGCXpMr3XYbuIwUd1xNOPDSeOxPCCCSwIzZ'
-ACCESS_TOKEN_SECRET = '8d8O2ZjuoTbHCoJqpYxd13IUGAPwOc2DOkuXjKtqxYhrX'
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
 
 def oauth_login(consumer_key, consumer_secret,access_key,access_secret):
   """Authenticate with twitter using OAuth"""
@@ -22,35 +22,44 @@ if __name__ == "__main__":
   api = oauth_login(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
   print "Authenticated as: %s" % api.me().screen_name
 
-  ltweets     = []
-  ldiputados  = []
+  l_tweets   = []
+  l_targets  = []
 
-  atweets     = str(sys.argv[1])
-  adiputados  = str(sys.argv[2])
+  arg_tweets  = str(sys.argv[1])
+  arg_target  = str(sys.argv[2])
+  print arg_tweets
+  print arg_target
 
-  ftweets     = open(atweets,'r')
-  fdiputados  = open(adiputados,'r')
+  f_tweets     = open(arg_tweets,'r')
+  f_targets  = open(arg_target,'r')
 
-  stweets     = ftweets.readlines()
-  sdiputados  = fdiputados.readlines()
+  s_tweets     = f_tweets.readlines()
+  s_targets  = f_targets.readlines()
+  
 
-  for line in stweets:
-    ltweets.append(line.strip())
+  # crear lista de targets
+  for line in s_targets:
+    l_targets.append(line.strip().decode('utf-8'))
 
-  for line in sdiputados:
-    ldiputados.append(line.strip())
+  # crear lista de tweets
+  for line in s_tweets:
+    l_tweets.append(line.strip().decode('utf-8'))
 
-  for diputado in ldiputados:
-    random.shuffle(ltweets)
-    for tweet in ltweets:
-      tweet   = tweet.decode('utf-8')
-      tweet   = tweet.replace('@',diputado)
-      cadena  = tweet
+
+  # crear y enviar tweets
+  for tweet in l_tweets:
+    actual_target = '@'
+    for target in l_targets:
+      tweet   = tweet.replace(actual_target, target)
+      if target != '':
+        actual_target = target
       print tweet
+      
       try:
-          api.update_status(status=cadena)
-          espera = randint(4,20)
-          time.sleep(espera)
-          print 'espera: '+str(espera)+' segundos'
+        api.update_status(status=tweet)
+        espera = randint(4,20)
+        time.sleep(espera)
+        print 'espera: '+str(espera)+' segundos'
       except:
-          print "Failed to tweet:", tweet
+        print "Failed to tweet:", tweet
+      
